08838a0f30ead8f7932ab87ab86a9096feb413d0
Michal Lekstan <m.lekstan@interia.pl>
2025-02-14 00:44:36 +0100

    876-Middle_of_the_Linked_List\876-Middle_of_the_Linked_List_1.py

---
+++ Easy/876-Middle_of_the_Linked_List/876-Middle_of_the_Linked_List_1.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
